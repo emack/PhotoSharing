b09874154ccc1781945a58c18eb84ea7d3c32345
edmar-trimorph <edmar.posesano@trimor.ph>
2015-03-14 23:10:04 +0800

    test

---
+++ PhotoSharing/FeedCell.swift
@@ -0,0 +1,16 @@
+//
+//  FeedCell.swift
+//  PhotoSharing
+//
+//  Created by Edmar P. on 3/14/15.
+//  Copyright (c) 2015 Edmar P. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+}
